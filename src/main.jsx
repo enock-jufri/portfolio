ac1b2a7761988d57244a35219c43e30fff41ece5
enock-jufri <enockjufri@gmail.com>
2025-04-11 21:05:37 +0300

    nested route fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import routes from './routes.jsx'
-import { createHashRouter, RouterProvider } from 'react-router-dom'; 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
-const router = createHashRouter(routes);
+const router=createBrowserRouter(routes)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);

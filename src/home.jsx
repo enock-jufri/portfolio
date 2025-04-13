06f5751242f41e6c40030edcf172855cceedb3a7
enock-jufri <enockjufri@gmail.com>
2025-04-13 16:30:30 +0300

    projects page

--- src/home.jsx
+++ src/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { TypeAnimation } from 'react-type-animation';
 import { FaXTwitter,FaGithub,FaLinkedin } from 'react-icons/fa6'; // X (Twitter) logo
 
 
 const Home = () => {
+  const navigate=useNavigate()
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-row w-full items-center justify-center gap-[72px] ">
@@ -37,16 +39,16 @@ const Home = () => {
               />
             </h1>
             <p className="text-gray-300 mb-6 text-[32px]">A full stack software developer</p>
-            <button className="mt-10 px-5 py-2.5 bg-blue-600 hover:bg-blue-700 text-black rounded-md font-black">
+            <button className="mt-10 px-5 py-2.5 bg-blue-600 hover:bg-blue-700 text-black rounded-md font-black" onClick={()=>navigate('/projects')}>
               MY WORK
             </button>
           </div>
 
           {/* Social Icons */}
           <div className="flex gap-4 mb-2 text-gray-400">
-            <a href="#" className="hover:text-[#2563EB]"><FaXTwitter size={40} /></a>
-            <a href="#" className="hover:text-[#2563EB]"><FaGithub size={40} /></a>
-            <a href="#" className="hover:text-[#2563EB]"><FaLinkedin size={40} /></a>
+            <a href="https://x.com/JufriEnock" className="hover:text-[#2563EB]"><FaXTwitter size={40} /></a>
+            <a href="https://github.com/enock-jufri" className="hover:text-[#2563EB]"><FaGithub size={40} /></a>
+            <a href="https://www.linkedin.com/in/enock-koech-651181208?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BchPiHML2S5%2BVQh5oUj%2BDLA%3D%3D" className="hover:text-[#2563EB]"><FaLinkedin size={40} /></a>
           </div>
         </div>
       </div>
--- src/projects.jsx
+++ src/projects.jsx
@@ -1,6 +1,96 @@
-const Project=()=>{
-    return(
-        <h1>projects</h1>
+import { SiPython, SiReact, SiJavascript } from 'react-icons/si'
+const Project = () => {
+    let projects = [
+        {
+            name: "MODERN BANKING",
+            synopsis: "A modern way to manage your money",
+            description: "A sleek, secure platform for managing personal and group finances. " +
+                "Users can check balances, transfer funds, apply for loans, and track savings — all in one place. " +
+                "Designed for simplicity, speed, and mobile-first access.",
+            image1: " ",
+            image2: " ",
+            github: "https://github.com/enock-jufri/bank_ui",
+            website: "https://bank-ui-delta.vercel.app/"
+        },
+        {
+            name: "OSIEPE SAVINGS",
+            synopsis: "A modern way for Chama group management",
+            description: "A smart platform for managing personal and group savings." +
+                " Track contributions, monitor group goals, and stay on top of your finances — all in one place.",
+            image1: " ",
+            image2: " ",
+            github: "",
+            website: ""
+        },
+        {
+            name: "EKL NEWS",
+            synopsis: "Read the latest news articles on the go",
+            description: "Stay informed with the latest headlines, in-depth stories, and real-time updates from around the world." +
+                "A modern news platform designed for clarity, speed, and reliability.",
+            image1: " ",
+            image2: " ",
+            github: "https://github.com/enock-jufri/EKL-news",
+            website: "https://ekl-news.vercel.app/"
+        },
+        {
+            name: "MOVIE EXPLORER",
+            synopsis: "Browse and download the latest movies",
+            description: "Discover, stream, and download your favorite movies effortlessly. " +
+                "MovieExplorer offers a sleek, user-friendly interface to explore films by genre, rating, or release — all in one place.",
+            image1: " ",
+            image2: " ",
+            github: "https://github.com/enock-jufri/movieExplorer",
+            website: "https://movie-explorer-sepia.vercel.app/"
+        },
+
+
+    ]
+    return (
+
+        <div className=" grid grid-cols-2 gap-5">
+            {
+                projects.map((Project, index) => (
+                    <div key={index} className="p-4 rounded-2xl bg-gray-800 shadow-md">
+                        <div className="h-5 rounded w-1/2 mb-1 font-bold">
+                            {Project.name}
+                        </div>
+                        <div className="h-4 rounded w-1/2 mb-3">{Project.synopsis}</div>
+                        <div className="flex h-48 rounded-xl mb-4 gap-5">
+                            <div className="bg-gray-700 w-1/2 rounded-xl" >
+                                <img
+                                    className="object-cover rounded-xl w-full h-full"
+                                    src="https://images.unsplash.com/photo-1634084462412-b54873c0a56d?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    alt="" />
+                            </div>
+                            <div className="bg-gray-700 w-1/2 rounded-xl" >
+                                <img
+                                    className="object-cover rounded-xl w-full h-full"
+                                    src="https://images.unsplash.com/photo-1634084462412-b54873c0a56d?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    alt="" />
+                            </div>
+
+                        </div>
+                        <div className="flex gap-2 h-10 rounded mb-2">
+                            <div><SiPython className="text-[#3776AB] text-4xl" size={30} /></div>
+                            <div><SiReact className="animate-spin text-[#61DAFB] text-5xl"
+                                style={{ animationDuration: "3s" }} size={30} /></div>
+                            <div><SiJavascript className="text-[#F7DF1E] text-4xl" size={30} /></div>
+
+                        </div>
+                        <div className="h-10 rounded mb-4">
+                            {Project.description}
+                        </div>
+                        <div className="flex rounded w-1/2 mb-0 gap-5">
+                            <a className="text-blue-500 hover:text-blue-600" href={Project.website}>Website</a>
+                            <a className="text-blue-500 hover:text-blue-600" href={Project.github}>Github repo</a>
+                        </div>
+                    </div>
+
+                ))
+            }
+        </div>
+
+
     )
 }
 
